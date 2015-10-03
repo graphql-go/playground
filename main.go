0a2da6792a17a3a0b6c52712f8abe7a476b6cf5c
Hafiz Ismail <sgk.sprm@gmail.com>
2015-10-03 20:17:38 +0800

    Updated to use `graphql-go-handler`

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/sogko/graphql-relay-go"
+	"github.com/sogko/graphql-go-handler"
 	"github.com/sogko/graphql-relay-go/examples/starwars"
 )
 
@@ -11,7 +11,7 @@ func main() {
 
 	// simplest relay-compliant graphql server HTTP handler
 	// using Starwars schema from `graphql-relay-go` examples
-	h := gqlrelay.NewHandler(&gqlrelay.HandlerConfig{
+	h := gqlhandler.New(&gqlhandler.Config{
 		Schema: &starwars.Schema,
 		Pretty: true,
 	})
